a665d0b742bea4681be1ad0499ba850a168e4d02
lsyf <linlsyf@gmail.com>
2018-04-19 09:04:10 +0800

    修改包名gradle

---
+++ lib/src/main/java/com/easysoft/dynamicrecycleview/TestView.java
@@ -0,0 +1,11 @@
+package com.easysoft.dynamicrecycleview;
+
+/**
+ * Created by lindanghong on 2018/4/18.
+ */
+
+public class TestView {
+    public String get(){
+        return "test sucess";
+    }
+}
