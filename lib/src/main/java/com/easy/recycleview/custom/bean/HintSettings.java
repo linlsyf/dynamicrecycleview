73e1b935ce3c310a8e1c2c383581c4b048add677
linlsyf <linlsyf@gmail.com>
2019-05-01 23:00:42 +0800

    修改默认标题设置

--- lib/src/main/java/com/easy/recycleview/custom/bean/HintSettings.java
+++ lib/src/main/java/com/easy/recycleview/custom/bean/HintSettings.java
@@ -1,13 +1,26 @@
 package com.easy.recycleview.custom.bean;
 
-import com.easy.recycleview.custom.baseview.item.ContentItemView;
-
 /**
  * Created by Administrator on 2019/5/1 0001.
  */
 
 public class HintSettings {
-    public static void load(ContentItemView itemView, DyItemBean dataItemBean) {
+    float textSize=16;
+    int color=0;
 
+    public float getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(float textSize) {
+        this.textSize = textSize;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
     }
 }
